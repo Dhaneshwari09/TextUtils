67ea5559ba09e73d33067837cffa91d32a307df9
Dhaneshwari Patel <itzrakhi2005@gmail.com>
2025-02-07 00:56:13 +0530

    Updates

---
+++ precache-manifest.14b1cc8a19f1da4044cec7b5214a7962.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a89e5f88a1f5c1d733b24de154b582d",
+    "url": "/TextUtils/index.html"
+  },
+  {
+    "revision": "af0533dde0639b3439e7",
+    "url": "/TextUtils/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/TextUtils/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "8e3a127b3eb410b6d841",
+    "url": "/TextUtils/static/js/3.445915d0.chunk.js"
+  },
+  {
+    "revision": "af0533dde0639b3439e7",
+    "url": "/TextUtils/static/js/main.7e4e256a.chunk.js"
+  },
+  {
+    "revision": "31ed193e8c0a0983812b",
+    "url": "/TextUtils/static/js/runtime~main.46b7b369.js"
+  }
+]);
